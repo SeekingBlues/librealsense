662af979b0bc4ed03044722d7158f8d3c4018c03
SeekingBlues <seekingblues@gmail.com>
2022-10-16 14:31:58 -0400

    Use the latest libusb

--- CMake/external_libusb.cmake
+++ CMake/external_libusb.cmake
@@ -3,10 +3,8 @@ include(ExternalProject)
 ExternalProject_Add(
     libusb
 
-    # Work-around for libusb master broken on Nov 26' 2020 with introduction of v1.0.24
-    # the issue has been reported in https://github.com/libusb/libusb/issues/812
-    GIT_REPOSITORY "https://github.com/ev-mp/libusb.git"
-    GIT_TAG "2a7372db54094a406a755f0b8548b614ba8c78ec" # "v1.0.22" + Mac get_device_list hang fix
+    GIT_REPOSITORY "https://github.com/libusb/libusb"
+    GIT_TAG "4239bc3a50014b8e6a5a2a59df1fff3b7469543b" # 1.0.26
 
     UPDATE_COMMAND ${CMAKE_COMMAND} -E copy_if_different
             ${CMAKE_CURRENT_SOURCE_DIR}/third-party/libusb/CMakeLists.txt
@@ -19,6 +17,7 @@ ExternalProject_Add(
             -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
             -DANDROID_ABI=${ANDROID_ABI}
             -DANDROID_STL=${ANDROID_STL}
+            -DANDROID_PLATFORM=${ANDROID_PLATFORM}
             -DCMAKE_INSTALL_PREFIX=${CMAKE_CURRENT_BINARY_DIR}/libusb_install
     TEST_COMMAND ""
     BUILD_BYPRODUCTS ${CMAKE_CURRENT_BINARY_DIR}/libusb_install/lib/${CMAKE_STATIC_LIBRARY_PREFIX}usb${CMAKE_STATIC_LIBRARY_SUFFIX}
